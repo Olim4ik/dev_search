233039529a1fa462e3a0aadae5a33e45d49ba97f
Olim4ik <odjavharov2@gmail.com>
2023-01-31 18:03:52 +0500

    many staff email + pas reset

--- users/views.py
+++ users/views.py
@@ -7,7 +7,7 @@ from django.db.models import Q
 from .models import Profile, Skill, Message
 from .utils import searchProfiles, paginateProfiles
 # from django.contrib.auth.forms import UserCreationForm # todo => replaced with below
-from .forms import CustomUserCreationForm, ProfileForm, SkillForm
+from .forms import CustomUserCreationForm, ProfileForm, SkillForm, MessageForm
 
 
 def loginUser(request):
@@ -177,4 +177,31 @@ def viewMessage(request, pk):
 
     context = {'message': message}
     return render(request, 'users/message.html', context)
+
+
+def createMessage(request, pk):
+    recipient = Profile.objects.get(id=pk)
+    form = MessageForm()
+
+    try:
+        sender = request.user.profile
+    except:
+        sender = None
+
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.sender = sender
+            message.recipient = recipient
+
+            if sender:
+                message.name = sender.name
+                message.email = sender.email
+            message.save()
+            messages.success(request, 'Your message was successfully sent!')
+            return redirect('user-profile', pk=recipient.id)
+
+    context = {'recipient': recipient, 'form': form}
+    return render(request, 'users/message_form.html', context)
 
